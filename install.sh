7170ca69322983c33234a1fd98ca750570af7c06
ysycloud <624533584@qq.com>
2016-04-12 22:29:44 +0800

    install

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#git clone
+git clone https://github.com/ysycloud/paraGSEA.git
+cd paraGSEA
+#make
+make all
+#install
+make install
+#clean
+make clean
+#Test
+quick_search_serial "data/data_for_test.txt" 10
--- runParaGSEALinux.sh
+++ runParaGSEALinux.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 #param list :filename topn
-#./bin/quick_search_serial "data/data_for_test.txt" 10
+#quick_search_serial "data/data_for_test.txt" 10
 
 #param list :filename thread_num topn paraway(0/1)
-#./bin/quick_search_omp "data/data_for_test.txt" 4 10 1
+#quick_search_omp "data/data_for_test.txt" 4 10 1
 
 #param list :process_num pernum hostfile filename topn
-#mpirun -n 2 -ppn 2 -hostfile hostfile ./bin/quick_search_mpi "data/data_for_test.txt" 15
+#mpirun -n 2 -ppn 2 -hostfile hostfile quick_search_mpi "data/data_for_test.txt" 15
 
 #param list :process_num pernum hostfile thread_num siglen filename1 filename2 outfilename
-#mpirun -n 2 -ppn 2 -hostfile hostfile ./bin/ES_Matrix_ompi_nocom 4 50 "data/data_for_test.txt" "data/data_for_test.txt" "data/ES_Matrix_test"
+#mpirun -n 2 -ppn 2 -hostfile hostfile ES_Matrix_ompi_nocom 4 50 "data/data_for_test.txt" "data/data_for_test.txt" "data/ES_Matrix_test"
 
 #param list :process_num pernum hostfile thread_num siglen filename1 filename2 outfilename
-#mpirun -n 2 -ppn 2 -hostfile hostfile ./bin/ES_Matrix_ompi_p2p 4 50 "data/data_for_test.txt" "data/data_for_test.txt" "data/ES_Matrix_test"
+#mpirun -n 2 -ppn 2 -hostfile hostfile ES_Matrix_ompi_p2p 4 50 "data/data_for_test.txt" "data/data_for_test.txt" "data/ES_Matrix_test"
 
 #param list :process_num pernum hostfile thread_num siglen filename1 filename2 outfilename
-#mpirun -n 2 -ppn 2 -hostfile hostfile ./bin/ES_Matrix_ompi_cocom 4 50 "data/data_for_test.txt" "data/data_for_test.txt" "data/ES_Matrix_test"
+#mpirun -n 2 -ppn 2 -hostfile hostfile ES_Matrix_ompi_cocom 4 50 "data/data_for_test.txt" "data/data_for_test.txt" "data/ES_Matrix_test"
 
 #param list :process_num pernum hostfile thread_num cluster_num filename outfilename
-#mpirun -n 2 -ppn 2 -hostfile hostfile ./bin/Cluster_KMediods_ompi 4 12 "data/ES_Matrix_test" "data/Cluster_result_test.txt"
+#mpirun -n 2 -ppn 2 -hostfile hostfile Cluster_KMediods_ompi 4 12 "data/ES_Matrix_test" "data/Cluster_result_test.txt"
 
 #param list :process_num pernum hostfile thread_num cluster_num filename outfilename
-#mpirun -n 2 -ppn 2 -hostfile hostfile ./bin/Cluster_KMediods++_ompi 4 12 "data/ES_Matrix_test" "data/Cluster_result_test.txt"
+#mpirun -n 2 -ppn 2 -hostfile hostfile Cluster_KMediods++_ompi 4 12 "data/ES_Matrix_test" "data/Cluster_result_test.txt"
