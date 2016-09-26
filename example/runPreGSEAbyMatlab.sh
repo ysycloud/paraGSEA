3bfe1bc697083591bcf244c51e1f2ec722f0fd7f
ysycloud <624533584@qq.com>
2016-09-26 17:30:28 +0800

    modify preGSEA

--- example/runPreGSEAbyMatlab.sh
+++ example/runPreGSEAbyMatlab.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 
 # execute matlab script to parse the data  
-matlab -nodesktop -nosplash -nojvm -r "file_input='../data/modzs_n272x978.gctx'; file_name='../data/data_for_test.txt'; file_name_cid='../data/data_for_test_cid.txt'; file_name_rid='../data/data_for_test_rid.txt'; PreGSEA; quit;"
+matlab -nodesktop -nosplash -nojvm -r "file_input='../data/modzs_n272x978.gctx'; file_name='../data/data_for_test.txt'; file_name_cidnum='../data/data_for_test_cidnum.txt'; PreGSEA; quit;"
 
 
+#matlab -nodesktop -nosplash -nojvm -r "PreGSEA; quit;"
+
+cat ../data/tmp >> ../data/data_for_test.txt
+rm -f ../data/tmp
--- matlab_for_parse/PreGSEA.m
+++ matlab_for_parse/PreGSEA.m
@@ -1,34 +1,68 @@
+if ~exist('file_input')
+	file_input='../data/modzs_n272x978.gctx';
+end
+
+if ~exist('file_name')
+	file_name='../data/data_for_test.txt';
+end
+
+if ~exist('file_name_cidnum')
+	file_name_cidnum='../data/data_for_test_cidnum.txt';
+end
+
+isCell = 0;
+if ~exist('cell_id_set') %not set, cell will not be consider
+	isCell = 1;
+end
+
+isPert = 0;
+if ~exist('pert_set') %not set, perturbation will not be consider
+	isPert = 1;
+end
+
+isDura = 0;
+if ~exist('duration') %not set, duration will not be consider
+	isDura = 1;
+end
+
+isCon = 0;
+if ~exist('concentration') %not set, concentration will not be consider
+	isCon = 1;
+end
 
 ds = parse_gctx(file_input);
 mat = ds.mat;
 cid = ds.cid;
-rid = ds.rid;
 [m,n]=size(ds.mat);
 
 probe=1:m;            %probe index
 probe=probe'; 
    
-fid1 = fopen(file_name, 'w');  %file point
-fid2 = fopen(file_name_cid, 'w');  %file point
-fid3 = fopen(file_name_rid, 'w');  %file point
-fprintf(fid1,'%10g\t%10g\n', n,m);
+fid1 = fopen('../data/tmp', 'w');  
+fid2 = fopen(file_name_cidnum, 'w');  
+fid3 = fopen(file_name, 'w');
+
 tic
-%pre-sort
+count = 0;
+cid_tmp = regexp(cid,':','split');
+%pre-sort and extract the fit profile
 o=ones(m,2);
 for i = 1:n
-    o = [mat(:,i),probe];
-    o = sortrows(o,1);
-    for j = 1:m-1
-         fprintf(fid1,'%5g\t',o(j,2));
-    end
-    fprintf(fid1,'%5g\n',o(m,2));
-	fprintf(fid2,'%s\n',char(cid(i)));
+	cid_info{i} = regexp(cid_tmp{i},'_','split');
+	if ( isCell || ismember(cid_info{i}{1}{2},cell_id_set) ) && ( isPert || ismember(cid_info{i}{2}{1},pert_set) ) && ( isDura || isequal(cid_info{i}{1}{3},duration) ) && ( isCon || isequal(cid_info{i}{3}{1},concentration) )
+		count = count+1;  %count the number of fit profile
+		o = [mat(:,i),probe];
+		o = sortrows(o,1);
+		for j = 1:m-1   %write out the profile
+			fprintf(fid1,'%5g\t',o(j,2));
+		end
+		fprintf(fid1,'%5g\n',o(m,2));
+		fprintf(fid2,'%d\n',i);   %write out cid number
+	end
 end
-for i = 1:m
-	fprintf(fid3,'%d->%s\n',i,char(rid(i)));
-end
+fprintf(fid3,'%10g\t%10g\n', count,m);
 toc
+
 fclose(fid1);
 fclose(fid2);
 fclose(fid3);
-
