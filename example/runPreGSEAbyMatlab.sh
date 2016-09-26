b3e5e7e6af0633171f15eed465a10bc2f150543b
ysycloud <624533584@qq.com>
2016-09-26 20:22:55 +0800

    paraPreGSEA

--- example/runPreGSEAbyMatlab.sh
+++ example/runPreGSEAbyMatlab.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
 # execute matlab script to parse the data  
-matlab -nodesktop -nosplash -nojvm -r "file_input='../data/modzs_n272x978.gctx'; file_name='../data/data_for_test.txt'; file_name_cidnum='../data/data_for_test_cidnum.txt'; PreGSEA; quit;"
+#matlab -nodesktop -nosplash -nojvm -r " file_input='../data/modzs_n272x978.gctx';  file_name='../data/data_for_test.txt';  file_name_cidnum='../data/data_for_test_cidnum.txt';  cell_id_set={'A549','MCF7','A375','A673','AGS'};  pert_set={'BRD-A51714012-001-04-9','BRD-A51714012-001-03-1','BRD-U88459701-000-01-8'};  duration = '6H';  concentration='10';  PreGSEA;  quit;"
 
-
-#matlab -nodesktop -nosplash -nojvm -r "PreGSEA; quit;"
+# execute matlab script to parse the data by acquiescent parameters
+matlab -nodesktop -nosplash -nojvm -r "PreGSEA; quit;"
 
 cat ../data/tmp >> ../data/data_for_test.txt
 rm -f ../data/tmp
--- matlab_for_parse/paraPreGSEA.m
+++ matlab_for_parse/paraPreGSEA.m
@@ -1,8 +1,42 @@
+if ~exist('file_input')
+	file_input='../data/modzs_n272x978.gctx';
+end
+
+if ~exist('file_name')
+	file_name='../data/data_for_test.txt';
+end
+
+if ~exist('file_name_cidnum')
+	file_name_cidnum='../data/data_for_test_cidnum.txt';
+end
+
+if ~exist('cores')
+	cores=2;
+end
+
+isCell = 0;
+if ~exist('cell_id_set') %not set, cell will not be consider
+	isCell = 1;
+end
+
+isPert = 0;
+if ~exist('pert_set') %not set, perturbation will not be consider
+	isPert = 1;
+end
+
+isDura = 0;
+if ~exist('duration') %not set, duration will not be consider
+	isDura = 1;
+end
+
+isCon = 0;
+if ~exist('concentration') %not set, concentration will not be consider
+	isCon = 1;
+end
 
 ds = parse_gctx(file_input);
 mat = ds.mat;
 cid = ds.cid;
-rid = ds.rid;
 [m,n]=size(ds.mat);
 
 probe=1:m;            %probe index
@@ -15,15 +49,19 @@ else
     disp('Already initialized');
 end
 
-fid = fopen(file_name, 'w');  %file point
-fid3 = fopen(file_name_rid, 'w');  %file point
-fprintf(fid,'%10g\t%10g\n', n,m);
+
 tic
 %pre-sort
 local = floor(n/cores);
 leave = mod(n,cores);
 fid1 = ones(1,cores);
-fid2 = ones(1,cores); 
+fid2 = ones(1,cores);
+lcount = zeros(1,cores);
+ 
+cid_tmp = regexp(cid,':','split');
+for i=1:n
+	cid_info{i} = regexp(cid_tmp{i},'_','split');
+end
 
 parfor i=1:cores
 	o=ones(m,2);
@@ -36,25 +74,30 @@ parfor i=1:cores
 		begin = (i-1)*local_n+leave;
 	end
 	fid1(i) = fopen(sprintf('%s_%d',file_name,i), 'w');
-	fid2(i) = fopen(sprintf('%s_%d',file_name_cid,i), 'w');
+	fid2(i) = fopen(sprintf('%s_%d',file_name_cidnum,i), 'w');
+	count = 0;
+	
+	%pre-sort and extract the fit profile in local thread
 	for j = 1:local_n
-		o = [mat(:,begin+j),probe];
-		o = sortrows(o,1);
-		for k = 1:m-1
-			fprintf(fid1(i),'%5g\t',o(k,2));
+		if ( isCell || ismember(cid_info{begin+j}{1}{2},cell_id_set) ) && ( isPert || ismember(cid_info{begin+j}{2}{1},pert_set) ) && ( isDura || isequal(cid_info{begin+j}{1}{3},duration) ) && ( isCon || isequal(cid_info{begin+j}{3}{1},concentration) )
+			lcount(i)=lcount(i)+1;	 %count the number of fit profile in local thread
+			o = [mat(:,begin+j),probe];
+			o = sortrows(o,1);
+			for k = 1:m-1	%write out the profile
+				fprintf(fid1(i),'%5g\t',o(k,2));
+			end
+			fprintf(fid1(i),'%5g\n',o(m,2));
+			fprintf(fid2(i),'%d\n',begin+j);	 %write out cid number
 		end
-		fprintf(fid1(i),'%5g\n',o(m,2));
-		fprintf(fid2(i),'%s\n',char(cid(begin+j)));
 	end
 	fclose(fid1(i));
 	fclose(fid2(i));
 end
 
-for i = 1:m
-	fprintf(fid3,'%d->%s\n',i,char(rid(i)));
-end
+count = sum(lcount);
+fid = fopen(file_name, 'w');  %file point
+fprintf(fid,'%10g\t%10g\n', count,m);
+fclose(fid);
 toc
 
-fclose(fid);
-fclose(fid3);
 matlabpool close;
