1de268a054da92fa9334a1b9680108b453e66e59
ysycloud <624533584@qq.com>
2016-09-29 19:39:12 +0800

    runreference new

--- example/runGenReference.sh
+++ example/runGenReference.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 # execute matlab script to generate reference directory
-#matlab -nodesktop -nosplash -nojvm -r " datasource='../data/modzs_n272x978.gctx'; genReferenceforNewDataSet; quit; "
+#matlab -nodesktop -nosplash -nojvm -r " datasource='../data/modzs_n272x978.gctx'; gene_symbol_rhd = 'pr_gene_symbol'; sample_conditions_chd = {'cell_id', 'pert_iname', 'pert_type', 'pert_itime', 'pert_idose'}; genReferenceforNewDataSet; quit; "
 
 # execute matlab script to generate reference directory by acquiescent parameters
 matlab -nodesktop -nosplash -nojvm -r " genReferenceforNewDataSet; quit;"
--- matlab_for_parse/genReferenceforNewDataSet.m
+++ matlab_for_parse/genReferenceforNewDataSet.m
@@ -1,45 +1,60 @@
+if ~exist('gene_symbol_rhd')
+	gene_symbol_rhd = 'pr_gene_symbol';
+end
+
+if ~exist('sample_conditions_chd')
+	sample_conditions_chd = {'cell_id', 'pert_iname', 'pert_type', 'pert_itime', 'pert_idose'}; 
+end
+
 if ~exist('datasource')
 	datasource='../data/modzs_n272x978.gctx';
 end
 
-load annot.mat;
-annot_info = regexp(annot,'\t','split');
-clear annot
-%extract GeneSymbol
-[at_m,~]=size(annot_info);    
-for i=1:at_m
-	annot_Symbol{i} = annot_info{i}{3};
-	annot_rid{i} = annot_info{i}{1};
-end
-
 ds= parse_gctx(datasource);
+%ds= parse_gct(datasource);
 [m,n]=size(ds.mat);
-ds_rid = ds.rid;
-ds_cid = ds.cid;
 
 %write out GeneSymbols corresponding to the rid in new dataset 
-[local,index] = ismember(annot_rid,ds_rid);
-o=ones(at_m,2);
-probe=1:at_m; 
-o=[index',probe'];
-o= sortrows(o,1);
+[risin,rindex] = ismember(gene_symbol_rhd, ds.rhd);
 fid1 = fopen('../data/Reference/Gene_List.txt', 'w');
 for i=1:m
-	j = at_m-m+i;
-	geneSymbol(i)=annot_Symbol(o(j,2));
-	fprintf(fid1,'%s\n',geneSymbol{i});
+	fprintf(fid1,'%s\n',ds.rdesc{i,rindex});
 end
 fclose(fid1);
 
-%write out cid/condition_info and line offset file in new dataset 
+%write out cid/condition_info and line offset file in new dataset
+[cisin,cindex] = ismember(sample_conditions_chd, ds.chd);
 fid2 = fopen('../data/Reference/Samples_Condition.txt', 'w');
 fid3 = fopen('../data/Reference/Samples_RowByteOffset.txt', 'w');
 offset = 0;
-cid_tmp = regexp(ds_cid,':','split');
 for i=1:n
-	cid_info{i} = regexp(cid_tmp{i},'_','split');
 	fprintf(fid3,'%10d\t',offset);
-	fprintf(fid2,'cid: %s;\tcell_line: %s;\tperturbation: %s;\tduration: %s;\tconcentration: %sum\n',ds_cid{i},cid_info{i}{1}{2},cid_info{i}{2}{1},cid_info{i}{1}{3},cid_info{i}{3}{1});
+	if cisin(1)==0
+		cell_line = 'field error';
+	else
+		cell_line = ds.cdesc{i,cindex(1)};
+	end
+	if cisin(2)==0
+		perturbation = 'field error';
+	else
+		perturbation = ds.cdesc{i,cindex(2)};
+	end
+	if cisin(3)==0
+		perturbation_type = 'field error';
+	else
+		perturbation_type = ds.cdesc{i,cindex(3)};
+	end
+	if cisin(4)==0
+		duration = 'field error';
+	else
+		duration = ds.cdesc{i,cindex(4)};
+	end
+	if cisin(5)==0
+		concentration = 'field error';
+	else
+		concentration = ds.cdesc{i,cindex(5)};
+	end
+	fprintf(fid2,'cell_line: %10s;    perturbation: %15s;    perturbation type: %10s;    duration: %10s;    concentration: %10s\n', cell_line, perturbation, perturbation_type, duration, concentration );
 	offset = ftell(fid2);
 end
 fclose(fid2);
