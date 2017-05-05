27ba448044c62e75aa61b44a731b94269291290a
ysycloud <624533584@qq.com>
2017-05-05 20:28:32 +0800

    resolve double free

--- src/Tools.c
+++ src/Tools.c
@@ -19,6 +19,7 @@ void split_data(int size, int n, int my_rank, int* begin, int* end, int* local_n
 	*end = *begin + *local_n;
 }
 
+
 //read the begin to end line part of profile file and get their triples
 //file:begin->end => triples: 0->local_P  
 void getTriples(int local_P, int genelen, int siglen, int profilenum, int linelen, int begin, int end,  char *file, struct Profile_triple * triples)
@@ -61,10 +62,63 @@ void getPartTriples(int genelen, int siglen, int profilenum, int linelen, int be
 		triples[i] = getTriple(profileSet[i-begin], genelen, siglen);
 	
 	//free the temp memory
+	for(i=0;i<len;i++)
+		free(profileSet[i]);
+	free(profileSet);		
+}
+
+/*
+//read the begin to end line part of profile file and get their triples  
+void getTriples(int local_P, int genelen, int siglen, int profilenum, int linelen, int begin, int end,  char *file, struct Profile_triple * triples)
+{
+	int i;
+	//allocate the temp memory
+	short **profileSet = (short **)malloc(local_P*sizeof(short *));
+	for(i=0;i<local_P;i++)
+		profileSet[i] = (short *)malloc(genelen*sizeof(short));	
+	short **tmp_profiles = (short **)malloc(profilenum*sizeof(short *));
+	for(i=0;i<profilenum;i++)
+		tmp_profiles[i] = (short *)malloc(genelen*sizeof(short));
+	
+	//read file and get the proper data
+	ReadFile(file, linelen, begin, end, profilenum, genelen, tmp_profiles);
+	for(i=0; i<local_P; i++)	
+		memcpy(profileSet[i],tmp_profiles[i+begin],genelen*sizeof(short));
+	
+	//get the triple for every profile	
+	for(i=0;i<local_P;i++)
+		triples[i] = getTriple(profileSet[i], genelen, siglen);
+	
+	//free the temp memory
+	for(i=0;i<local_P;i++)
+		free(profileSet[i]);
+	free(profileSet);
+	for(i=0;i<profilenum;i++)
+		free(tmp_profiles[i]);
+	free(tmp_profiles);		
+}
+//read the begin to end line part of profile file and get their triples in proper part of the  triples vector
+void getPartTriples(int genelen, int siglen, int profilenum, int linelen, int begin, int end,  char *file, struct Profile_triple * triples)
+{
+	int i;
+	//allocate the temp memory
+	short **profileSet = (short **)malloc(profilenum*sizeof(short *));
+	for(i=0;i<profilenum;i++)
+		profileSet[i] = (short *)malloc(genelen*sizeof(short));	
+	
+	//read file and get the proper data
+	ReadFile(file, linelen, begin, end, profilenum, genelen, profileSet);
+	
+	//get the triple for every profile	
+	for(i=begin;i<end;i++)
+		triples[i] = getTriple(profileSet[i], genelen, siglen);
+	
+	//free the temp memory
 	for(i=0;i<profilenum;i++)
 		free(profileSet[i]);
 	free(profileSet);		
 }
+*/
 
 //compare two set
 int cmpset(int *set1,int *set2,int n)
